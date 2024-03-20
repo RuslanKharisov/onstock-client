e6b98a08386e638d3950d2a6b9bb8a0b4feca954
RuslanKharisov <ruslan.kharisov@gmail.com>
2024-03-20 20:57:31 +0300

    table on main page

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,12 +1,11 @@
 import { Table } from "@/shared/ui/table";
 import { StockTable } from "@/widgets/stock-table/stock-table";
 
-
 export default async function Home() {
   return (
-    <main className="container flex min-h-screen flex-col  p-8">
+    <main className="container flex min-h-screen flex-col  p-8">      
       <h1 className="text-3xl mb-2">Продукция в ниличии на складах РФ</h1>
-      <StockTable/>
+      <StockTable />
     </main>
   );
 }
