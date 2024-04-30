89ff22f9ab23a7a9dd6b191e7add32edc5d1d763
RuslanKharisov <ruslan.kharisov@gmail.com>
2024-04-30 14:00:33 +0300

    prisma setup and start models

---
+++ prisma/migrations/20240430070943_add_new_models/migration.sql
@@ -0,0 +1,51 @@
+/*
+  Warnings:
+
+  - The primary key for the `Product` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The `id` column on the `Product` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+  - Added the required column `updatedAt` to the `Product` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Product" DROP CONSTRAINT "Product_pkey",
+ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL,
+DROP COLUMN "id",
+ADD COLUMN     "id" SERIAL NOT NULL,
+ADD CONSTRAINT "Product_pkey" PRIMARY KEY ("id");
+
+-- CreateTable
+CREATE TABLE "Supplier" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Supplier_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Stock" (
+    "id" SERIAL NOT NULL,
+    "supplierId" INTEGER NOT NULL,
+    "productId" INTEGER NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Stock_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Supplier_email_key" ON "Supplier"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Stock_supplierId_productId_key" ON "Stock"("supplierId", "productId");
+
+-- AddForeignKey
+ALTER TABLE "Stock" ADD CONSTRAINT "Stock_supplierId_fkey" FOREIGN KEY ("supplierId") REFERENCES "Supplier"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Stock" ADD CONSTRAINT "Stock_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,10 +1,19 @@
-import { Table } from "@/shared/ui/table";
+import { CreateProductForm } from "@/features/products-list/pub/create-product-form";
+import { ProductsList } from "@/features/products-list/pub/products-list";
+import { dbClient } from "@/shared/lib/db";
 import { StockTable } from "@/widgets/stock-table/stock-table";
 
+
 export default async function Home() {
+    const products = await dbClient.product.findMany();
+
+    console.log("🚀 ~ Home ~ products:", products)
+    
   return (
-    <main className="container flex min-h-screen flex-col  p-8">      
-      <h1 className="text-3xl mb-2">Продукция в ниличии на складах РФ</h1>
+    <main className="container flex min-h-screen flex-col  p-8">
+      <CreateProductForm revalidatePagePath="/" className={"w-1/3"} />
+      <ProductsList revalidatePagePath="/" />
+      <h2 className="text-3xl my-4">Продукция в наличии на складах РФ</h2>
       <StockTable />
     </main>
   );
