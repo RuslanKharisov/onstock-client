849b50dcbebc95ca0b7aaaf1e03693b11508cecd
RuslanKharisov <ruslan.kharisov@gmail.com>
2024-06-28 17:27:50 +0300

    документация к token repository

--- src/entities/user/_repositories/token.ts
+++ src/entities/user/_repositories/token.ts
@@ -2,16 +2,37 @@ import { dbClient } from "@/shared/lib/db"
 import { VerificationToken } from "../_domain/types"
 
 class TokenRepository {
-
-  async deleteVerificationToken (existingToken:VerificationToken) {
+  /**
+   * Асинхронно удаляет токен верификации из базы данных.
+   *
+   * @param existingToken - объект токена верификации, который необходимо удалить.
+   *
+   * Пример использования:
+   * ```
+   * const token = await getVerificationTokenByEmail('user@example.com');
+   * await deleteVerificationToken(token);
+   * ```
+   */
+  async deleteVerificationToken(existingToken: VerificationToken) {
     await dbClient.verificationToken.delete({
       where: {
-        id: existingToken.id
-      }
+        id: existingToken.id,
+      },
     })
   }
 
-   async getVerificationTokenByToken (token: string) {
+  /**
+   * Асинхронно извлекает уникальный токен верификации из базы данных по значению токена.
+   *
+   * @param token - строка, представляющая токен, который нужно найти.
+   * @returns {Promise<VerificationToken | null>} Возвращает объект токена верификации или null, если токен не найден.
+   *
+   * Пример использования:
+   * ```
+   * const verificationToken = await getVerificationTokenByToken('12345');
+   * ```
+   */
+  async getVerificationTokenByToken(token: string) {
     try {
       const verificationToken = await dbClient.verificationToken.findUnique({
         where: { token },
@@ -21,8 +42,21 @@ class TokenRepository {
       return null
     }
   }
-  
-   async getVerificationTokenByEmail  (email: string): Promise<VerificationToken | null> {
+
+  /**
+   * Асинхронно извлекает токен верификации из базы данных по адресу электронной почты.
+   *
+   * @param email - строка, представляющая электронную почту, связанную с токеном, который нужно найти.
+   * @returns {Promise<VerificationToken | null>} Возвращает объект токена верификации или null, если токен не найден.
+   *
+   * Пример использования:
+   * ```
+   * const verificationToken = await getVerificationTokenByEmail('user@example.com');
+   * ```
+   */
+  async getVerificationTokenByEmail(
+    email: string,
+  ): Promise<VerificationToken | null> {
     try {
       const verificationToken = await dbClient.verificationToken.findFirst({
         where: { email },
@@ -32,13 +66,6 @@ class TokenRepository {
       return null
     }
   }
-
 }
 
 export const tokenRepository = new TokenRepository()
-
-
-
-
-
-
