92309af81974ac698f7b41192de13e4d0e140042
RuslanKharisov <ruslan.kharisov@gmail.com>
2024-07-31 15:58:17 +0300

    tooltip for table

--- src/entities/producrts-list/_vm/_products-table-columns.tsx
+++ src/entities/producrts-list/_vm/_products-table-columns.tsx
@@ -1,9 +1,15 @@
 "use client"
 
 import { Button } from "@/shared/ui/button"
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/shared/ui/dropdown-menu"
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/shared/ui/tooltip"
 import { ColumnDef } from "@tanstack/react-table"
-import { ArrowUpDown, MoreHorizontal } from "lucide-react"
+import { ArrowUpDown, Earth, MailPlus, MoreHorizontal } from "lucide-react"
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
@@ -30,29 +36,44 @@ export const ProductsTableColumns: ColumnDef<Stock>[] = [
     header: () => "Поставщик",
   },
   {
+    header: () => "Ссылки",
     id: "actions",
     enableHiding: false,
     cell: ({ row }) => {
       const stock = row.original
       return (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" className="h-8 w-8 p-0">
-              <span className="sr-only">Открыть меню</span>
-              <MoreHorizontal className="h-4 w-4" />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuLabel>Действия</DropdownMenuLabel>
-            <DropdownMenuItem
-              onClick={() => navigator.clipboard.writeText(stock.email)}
-            >
-              Копировать Email поставщика
-            </DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem><a target="blank" href={`https://${stock.siteUrl}`}>Перейти на сайт поставщика</a></DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+        <span className="flex gap-1">
+          <TooltipProvider delayDuration={100}>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button size="icon" variant="link">
+                  <a target="blank" href={`https://${stock.siteUrl}`}>
+                    <Earth />
+                  </a>
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Перейти на сайт поставщика</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+          <TooltipProvider delayDuration={100}>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button
+                  variant="link"
+                  size="icon"
+                  onClick={() => navigator.clipboard.writeText(stock.email)}
+                >
+                  <MailPlus />
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Скопировать почту</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </span>
       )
     },
   },
