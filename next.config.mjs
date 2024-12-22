abf5890ddb04eaaa64f962f756001c1346dc8809
RuslanKharisov <ruslan.kharisov@gmail.com>
2024-12-22 16:15:22 +0300

    nextauth

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,3 @@
-const config = {
-  env: {
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-  },
-  async redirects() {
-    return [
-      {
-        source: '/auth/:path*',
-        destination: '/api/next-auth/:path*',
-        permanent: true,
-      },
-    ];
-  },
-};
+const config = {}
 
-export default config;
+export default config
