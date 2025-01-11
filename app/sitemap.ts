8f5cccbb8d69b5e53f3b6487285a99c0225254d7
RuslanKharisov <ruslan.kharisov@gmail.com>
2025-01-11 21:45:52 +0300

    sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,7 +5,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: 'https://prom-stock.ru/',
       lastModified: new Date(),
-      changeFrequency: 'yearly',
+      changeFrequency: 'monthly',
       priority: 1,
     },
     {
