c26b2cd7409489d03ba7701a48534e8cad85f350
RuslanKharisov <ruslan.kharisov@gmail.com>
2025-01-13 12:14:40 +0300

    link to www.barbarisstudio.ru

--- src/widgets/app-footer/_ui/footer.tsx
+++ src/widgets/app-footer/_ui/footer.tsx
@@ -3,22 +3,21 @@ import { LogoIcon } from "@/shared/ui/logo-icon"
 import { Telegram } from "@/shared/icons/telegram"
 import { WhatsApp } from "@/shared/icons/whatsapp"
 
-
-const Footer = () => {  
+const Footer = () => {
   const whatsLink = process.env.NEXT_WHATSAPP
-  const tgLink = process.env.NEXT_TELEGRAM 
-  
-  return(
-  <footer className="w-full py-8">
-    <div className="container">
-      <section className="flex flex-col items-center justify-between gap-y-3 lg:flex-row ">
-        <Link className="space-x-2" href="/">
-          <span className="inline-block font-bold">
-            <LogoIcon className="mr-3 h-6 w-6" />
-          </span>
-          Промышленный склад
-        </Link>
-        <div className="flex space-x-6">
+  const tgLink = process.env.NEXT_TELEGRAM
+
+  return (
+    <footer className="w-full py-8">
+      <div className="container">
+        <section className="flex flex-col items-center justify-between gap-y-3 lg:flex-row ">
+          <Link className="space-x-2" href="/">
+            <span className="inline-block font-bold">
+              <LogoIcon className="mr-3 h-6 w-6" />
+            </span>
+            Промышленный склад
+          </Link>
+          <div className="flex space-x-6">
             <a
               href={`https://api.whatsapp.com/send/?phone=${whatsLink}`}
               className=""
@@ -26,22 +25,26 @@ const Footer = () => {
             >
               <WhatsApp />
             </a>
-          <a href={tgLink} className="" aria-label="Напишите в Telegram">
-            <Telegram />
-          </a>
+            <a href={tgLink} className="" aria-label="Напишите в Telegram">
+              <Telegram />
+            </a>
+          </div>
+        </section>
+        <div className=" my-8 flex flex-col items-center gap-y-5 md:my-2">
+          <p className="text-center text-xs">
+            © 2024{" "}
+            <a
+              href="https://www.barbarisstudio.ru/"
+              className="hover:underline"
+            >
+              Studio Barbaris™
+            </a>
+            . All Rights Reserved.
+          </p>
         </div>
-      </section>
-      <div className=" my-8 flex flex-col items-center gap-y-5 md:my-2">
-        <p className="text-center text-xs">
-          © 2024{" "}
-          <a href="https://studiobarbaris.com" className="hover:underline">
-            Studio Barbaris™
-          </a>
-          . All Rights Reserved.
-        </p>
       </div>
-    </div>
-  </footer>
-)}
+    </footer>
+  )
+}
 
 export { Footer }
