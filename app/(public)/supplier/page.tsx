c0a08b741c2bedd65f017ab478a2051cea20acda
Ruslan Kharisov <ruslan.kharisov@gmail.com>
2025-03-25 12:43:35 +0300

    fix: баг генерации supplier

--- app/(public)/supplier/page.tsx
+++ app/(public)/supplier/page.tsx
@@ -1,6 +1,8 @@
 import { GetSupplierList } from "@/entities/supplier/api/get-all-suppliers"
 import { SupplierList } from "@/views/suppliers"
 
+export const dynamic = "force-dynamic"
+
 export default async function SuppliersPage() {
   const SuppliersListQuery = {
     page: 1,
@@ -8,7 +10,24 @@ export default async function SuppliersPage() {
     filters: { name: "" },
   }
 
-  const data = await GetSupplierList(SuppliersListQuery)
-
-  return <SupplierList suppliersData={data} />
+  try {
+    const data = await GetSupplierList(SuppliersListQuery)
+    return <SupplierList suppliersData={data} />
+  } catch {
+    return (
+      <SupplierList
+        suppliersData={{
+          data: [],
+          meta: {
+            total: 0,
+            currentPage: 1,
+            page: 10,
+            lastPage: 1,
+            next: null,
+            prev: null,
+          },
+        }}
+      />
+    )
+  }
 }
