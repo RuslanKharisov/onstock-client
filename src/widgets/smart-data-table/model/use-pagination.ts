f04bf42d63d7b8a345268989580b75c6a126ad26
Ruslan Kharisov <ruslan.kharisov@gmail.com>
2025-04-17 11:30:06 +0300

    usePagination default size 5

--- src/widgets/smart-data-table/model/use-pagination.ts
+++ src/widgets/smart-data-table/model/use-pagination.ts
@@ -3,14 +3,14 @@ import { useState } from "react";
 export const usePagination = () => {
   const [page, _setPage] = useState(1);
   const [perPage, _setLimit] = useState(10);
-  const [pagination, setPagination] = useState ({ 
-    pageSize : 10 , 
-    pageIndex : 0 , 
+  const [pagination, setPagination] = useState({
+    pageSize: 5,
+    pageIndex: 0,
   });
 
   return {
     skip: (page - 1) * perPage,
     pagination,
-    onPaginationChange : setPagination, 
+    onPaginationChange: setPagination,
   };
 };
